e6d26ad2604e1485dfb29f8ea407d5bfa39b04ff
Gaspard Jankowiak <gaspard.jankowiak@math.cnrs.fr>
2014-10-14 14:57:16 +0200

    complete numpy

--- rbs.py
+++ rbs.py
@@ -4,36 +4,45 @@
 from dolfin import *
 import numpy as np
 import utils
+import scipy.linalg as la
+import scipy.sparse as sp
+import scipy.sparse.linalg as spla
 
 from IPython import embed
 
-parameters['linear_algebra_backend'] = 'uBLAS'
 set_log_level(ERROR)
 
 randint = np.random.randint
 
+# Exceptions
+class RBConvergenceError(RuntimeError): pass
+class LinSolvConvergenceError(RuntimeError): pass
+class ParameterSpaceDepleted(RuntimeError): pass
+class IllConditionedBasis(RuntimeError): pass
+
 class AffineReducedBasisSolver(object):
 
-    def __init__(self, forms, factors, source, function_space, param_space, **options):
+    def __init__(self, forms, factors, source, function_space, param_space, bcs=None, **options):
 
         # Base problem is
         # Σ_i forms[i](param) * factors[i](u, v) = source(v)    ∀ v
 
-        self.forms          = forms            # FEniCS UFL forms
-        self.factors        = factors          # python functions
-        self.source         = assemble(source) # FEniCS UFL form
+        self.forms          = forms                    # FEniCS UFL forms
+        self.factors        = factors                  # python functions
+        self.source         = assemble(source, bcs=bcs).array() # FEniCS UFL form
         self.V              = function_space
         self.param_space    = param_space
-        self.N              = self.source.size()
-        self.n              = 10
+        self.bcs            = bcs
+        self.N              = self.source.size
+        self.n              = 0
 
         u, v = TrialFunction(self.V), TestFunction(self.V)
-        self.L2_prod        = assemble(u*v*dx)
+        self.L2_prod = sp.csr_matrix(assemble(u*v*dx, bcs=self.bcs).array())
 
         if options.has_key('inner_prod'):
-            self.inner_prod = assemble(options['inner_prod'])
+            self.inner_prod_mat = sp.csr_matrix(assemble(options['inner_prod'], bcs=self.bcs).array())
         else:
-            self.inner_prod = assemble((inner(grad(u), grad(v)) + u*v)*dx)
+            self.inner_prod_mat = sp.csr_matrix(assemble((inner(grad(u), grad(v)) + u*v)*dx, bcs=self.bcs).array())
 
 
         if len(factors) != len(forms):
@@ -41,26 +50,33 @@ class AffineReducedBasisSolver(object):
 
         self.matrices = []
         for f in self.forms:
-            self.matrices.append(assemble(f))
+            self.matrices.append(sp.csr_matrix(assemble(f, bcs=self.bcs).array()))
 
+    def spsolve(self, A, rhs):
+        sol = spla.spsolve(A, rhs)
+        return sol
+
+    def solve(self, A, rhs):
+        sol = la.solve(A, rhs, sym_pos=True)
+        return sol
 
     # methods:
     #   - 'ap'   : a posteriori estimator
     #   - 'pod'  : proper orthogonal decomposition
     #   - 'true' : true error
-    def reduce(self, tol, method='ap', n=10):
+    def reduce(self, tol, method='ap', basis_size=10, do_ortho=True, start_idc=None):
 
-        self.n = n
+        self.n = basis_size
+        self.do_ortho = do_ortho
 
-        self.s = uBLASKrylovSolver() #LinearSolver()
-        self.s.parameters["monitor_convergence"] = True
-        self.s.parameters['absolute_tolerance'] = 1e-13
+        param_space_size = reduce(lambda s, l:s*len(l), self.param_space, 1)
+        if self.n > param_space_size:
+            raise ValueError('basis_size ({0}) must be less than the size of the parameter space ({1})'.format(self.n, param_space_size))
+
         self.basis = []
 
-        self.rb_mats = [self.matrix(self.n) for r in self.forms]
-        for rb_mat in self.rb_mats:
-            rb_mat.ident(np.array(range(self.n), dtype=np.intc))
-        self.rb_rhs = self.vector(self.n)
+        self.rb_mats = [np.eye(self.n) for r in self.forms]
+        self.rhs_rb = np.zeros(self.n)
 
         if method != 'ap':
             raise NotImplemented('only a posteriori estimator is available for now')
@@ -68,18 +84,19 @@ class AffineReducedBasisSolver(object):
         self.s_idc = []
 
         # initialization
-        i_idx = []
-        for s in self.param_space:
-            i_idx.append(randint(len(s)))
-        self.s_idc.append(i_idx)
+        if start_idc is None:
+            i_idc = []
+            for s in self.param_space:
+                i_idc.append(randint(len(s)))
+        else:
+            i_idc = start_idc
+        self.s_idc.append(i_idc)
 
-        u0 = self.vector()
-        self.s.solve(self.bl_matrix(self.s_idc[self.k]), u0, self.source)
+        u0 = self.spsolve(self.bl_matrix(self.s_idc[self.k]), self.source)
         u0 = self.ortho(u0)
 
         # phi
-        self.phi = self.vector()
-        self.s.solve(self.inner_prod, self.phi, self.source)
+        self.phi = self.spsolve(self.inner_prod_mat, self.source)
 
         # psis
         self.psis = []
@@ -90,64 +107,58 @@ class AffineReducedBasisSolver(object):
         while True:
             err, idc = self.error_argmax()
             if err < tol: break
+            if self.k == self.n:
+                raise RBConvergenceError('failed to meet tolerance {0:.2e} with {1} basis functions'.format(tol, self.n))
 
-            u = self.vector()
-            self.s.solve(self.bl_matrix(idc), u, self.source)
+            u = self.spsolve(self.bl_matrix(idc), self.source)
             u = self.ortho(u)
 
             self.compute_psis(u)
             self.add(u)
+            self.s_idc.append(idc)
+
+        for ub in self.basis:
+            self.plot(ub)
+        self.cleanup()
+        return self.s_idc, self.basis
 
     def add(self, u):
         self.basis.append(u)
-        _ = self.vector(u.size())
         for i, m in enumerate(self.matrices):
             for j, ub in enumerate(self.basis):
-                m.mult(u, _)
-                bl_int = ub.inner(_)
+                bl_int = ub.dot(m*u)
                 # not very efficient
-                self.rb_mats[i].set(utils.f64_array(bl_int), utils.intc_array([j]), utils.intc_array([self.k-1]))
-                self.rb_mats[i].set(utils.f64_array(bl_int), utils.intc_array([self.k-1]), utils.intc_array([j]))
-        self.L2_prod.mult(u, _)
-        self.rb_rhs.add(utils.f64_array(u.inner(_)), utils.intc_array([self.k-1]))
+                self.rb_mats[i][j, self.k-1] = bl_int
+                self.rb_mats[i][self.k-1, j] = bl_int
+        self.rhs_rb[self.k-1] = u.dot(self.source)
 
     def error_argmax(self):
-        u_rb_comp = self.vector(self.n)
         idc_max = None
         err_max = -1
         for idc, params in utils.ml_iterator(self.param_space, values=True):
             if idc in self.s_idc: continue
             # solve problem on reduced basis
             M_rb = self.rb_matrix(idc)
-            self.s.solve(M_rb, u_rb_comp, self.rb_rhs)
-            u_rb_arr = u_rb_comp.array()
+            u_rb = self.solve(M_rb, self.rhs_rb)
             # assemble residual dual
-            residual = uBLASVector(self.phi)
+            residual = np.array(self.phi)
             for n in range(self.k):
                 for q in range(len(self.forms)):
-                    residual.add_local(self.factors[q](params)*u_rb_arr[n]* self.psis[n][q])
+                    residual -= self.factors[q](params)*u_rb[n]*self.psis[n][q]
             # compute error
             err = np.sqrt(self.inner(residual, residual))
-            print "Current error: {0}, k={1}".format(err_max, self.k)
             if err > err_max:
-                [err_max, idc_max] = [err, idc]
-        print "Current max error: {0}".format(err_max)
+                [err_max, idc_max] = [err, list(idc)]
+        if idc_max is None:
+            raise ParameterSpaceDepleted
+        print "Max error: {0}".format(err_max)
         return err_max, idc_max
-
-    def _test_solve(self, mat):
-        n = mat.size(0)
-        xx = self.vector(n)
-        rhs = self.vector(n)
-        rhs.array()[:] = np.random.random(n)
-        self.s.solve(mat, xx, rhs)
 
     def rb_matrix(self, params_idc):
         params = [self.param_space[i][params_idc[i]] for i in range(len(params_idc))]
         M = self.factors[0](params)*self.rb_mats[0]
         for i, m in enumerate(self.rb_mats[1:], 1):
             M = M + self.factors[i](params)*m
-        print params_idc, params
-        print M.array()
         return M
 
     def bl_matrix(self, params_idc, idx='all'):
@@ -160,23 +171,18 @@ class AffineReducedBasisSolver(object):
             M = self.factors[idx](params)*self.matrices[idx]
         return M
 
-    def vector(self, size=0):
-        return uBLASVector(size or self.N)
-
-    def matrix(self, size=0):
-        return uBLASDenseMatrix(size or self.N, size or self.N)
-
     def inner(self, u, v):
-        _ = self.vector(u.size())
-        self.inner_prod.mult(v, _)
-        return u.inner(_)
+        return u.dot(self.inner_prod_mat * v)
 
     def ortho(self, u):
-        un = self.vector(u)
-        for ub in self.basis:
-            un = un - self.inner(u, ub)*ub
-        un = un/sqrt(self.inner(un, un))
-        return un
+        un = np.array(u)
+        if self.do_ortho:
+            for ub in self.basis:
+                un = un - self.inner(u, ub)*ub
+        nrm = sqrt(self.inner(un, un))
+        if nrm < 1e-10:
+            raise IllConditionedBasis
+        return un/nrm
 
     def basis_function(self):
         basis_f = map(self.fe_function, self.basis)
@@ -189,15 +195,21 @@ class AffineReducedBasisSolver(object):
 
     def compute_psis(self, u):
         if self.n == self.k:
-            raise IndexError('Bilinear integral matrices are full, try calling reduce with a higher value for n')
+            raise RBConvergenceError
         psis_k = []
         for m in self.matrices:
-            psi_ki = self.vector()
-            rhs = self.vector()
-            m.mult(-u, rhs)
-            self.s.solve(self.inner_prod, psi_ki, rhs)
+            rhs = m*u
+            psi_ki = self.spsolve(self.inner_prod_mat, rhs)
             psis_k.append(psi_ki)
         self.psis.append(psis_k)
+
+    def plot(self, u):
+        f_u = Function(self.V)
+        f_u.vector()[:] = u
+        plot(f_u, interactive=True)
+
+    def cleanup(self):
+        self.phi = self.psis = self.rb_mats = self.rhs_rb = None
 
     @property
     def k(self):
--- test_rbs.py
+++ test_rbs.py
@@ -4,15 +4,20 @@ import numpy as np
 
 parameters['linear_algebra_backend'] = 'uBLAS'
 
-mesh = UnitSquareMesh(5, 5)
+mesh = UnitSquareMesh(50, 50)
 V = FunctionSpace(mesh, "P", 1)
 
 u, v = TrialFunction(V), TestFunction(V)
 
 a0 = inner(grad(u), grad(v))*dx
 a1 = u*v*dx
-f  = Expression('sin(x[0])')
+f  = Expression('10*sin(3*x[0])')
 source = f*v*dx
+
+def boundary(x, on_boundary):
+    return on_boundary
+
+bc = DirichletBC(V, Constant(0.0), boundary)
 
 def f0(params):
     return 1
@@ -20,7 +25,7 @@ def f0(params):
 def f1(params):
     return params[0]
 
-param_space = [np.linspace(0, 1, 5)]
+param_space = [np.linspace(1, 1000, 500)]
 
-solvr = rbs.AffineReducedBasisSolver([a0, a1], [f0, f1], source, V, param_space)
-solvr.reduce(1e-8)
+solvr = rbs.AffineReducedBasisSolver([a0, a1], [f0, f1], source, V, param_space, bcs=bc)
+solvr.reduce(1e-8, do_ortho=True, basis_size=20, start_idc=[0])
